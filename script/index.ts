fc689c710f56a82683e119e4fe9863945017a2c4
joewells96 <joe.wells@depop.com>
2024-02-14 11:23:33 +0000

    add eggs on click location

--- script/index.ts
+++ script/index.ts
@@ -1,13 +1,14 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const addEggButton: HTMLElement = document.getElementById('add-egg')
-    const pan: HTMLElement = document.getElementById('pan')
-    addEggButton.addEventListener('click', addEgg)
+import {toPx, formatSecondsToText} from './utils';
 
-    function addEgg(): void {
+window.addEventListener("DOMContentLoaded", () => {
+    const pan: HTMLElement = document.getElementById('pan')
+    document.addEventListener('click', addEgg)
+
+    function addEgg(event): void {
         const timeString: string = (<HTMLInputElement>document.getElementById('time')).value
         const egg: Egg = new Egg(timeString, `egg${eggs.length}`)
         eggs.push(egg)
-        pan.appendChild(egg.toHtml())
+        pan.appendChild(egg.toHtml(event.clientX, event.clientY))
         const intervalId: number = setInterval(() => updateEgg(egg, intervalId), 1000)
     }
 });
@@ -21,16 +22,11 @@ function updateEgg(egg: Egg, intervalId: number): void {
     if (secondsRemaining == 0) clearInterval(intervalId)
 }
 
-function formatSecondsToText(totalSeconds: number): string {
-    const minutes: number = Math.floor(totalSeconds / 60)
-    const seconds: number = totalSeconds % 60
-    const secondsString: string = seconds < 10 ? `0${seconds}` : `${seconds}`
-    return `${minutes}:${secondsString}`
-}
-
 class Egg {
     secondsRemaining: number;
     id: string;
+    private width: number = 136;
+    private height: number = 190;
 
     constructor(timeText: string, id: string) {
         const time: string[] = timeText.split(':')
@@ -43,10 +39,14 @@ class Egg {
         return this.secondsRemaining
     }
 
-    toHtml(): HTMLElement {
+    toHtml(clickX: number, clickY: number): HTMLElement {
         const eggHtml: HTMLElement = document.createElement('div')
         eggHtml.setAttribute('class', 'egg')
         eggHtml.setAttribute('id', this.id)
+        eggHtml.style.width = toPx(this.width)
+        eggHtml.style.height = toPx(this.height)
+        eggHtml.style.left = toPx(clickX - this.width / 2)
+        eggHtml.style.top = toPx(clickY - this.height / 2)
         eggHtml.textContent = formatSecondsToText(this.secondsRemaining)
         return eggHtml
     }
